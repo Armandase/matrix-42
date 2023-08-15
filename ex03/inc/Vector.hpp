d9f43de659bd74a613bfba0c1994b21eb085f555
Armandase <damiens.armand@gmail.com>
2023-08-15 13:38:48 +0200

    ex03 done, overflows checked

---
+++ ex03/inc/Vector.hpp
@@ -0,0 +1,34 @@
+#ifndef _VECTOR_HPP_
+# define _VECTOR_HPP_
+
+# include <vector>
+# include <ostream>
+
+typedef long unsigned int usize_t;
+
+template <typename K>
+class Vector{
+    public:
+        Vector(std::vector<K>);
+
+        std::vector<K> get_values() const;
+        usize_t get_size() const;
+
+        void    sub(Vector);
+        void    add(Vector);
+        void    scl(K);
+        K    dot(Vector);
+
+        Vector<K>& operator + (const Vector<K>&);
+		Vector<K>& operator - (const Vector<K>&);
+		Vector<K>& operator * (const K);
+    private:
+        bool isOverflow(K a, K b, char op);
+        
+        std::vector<K> _values;
+};
+
+template <typename K>
+std::ostream& operator<<(std::ostream& os, const Vector<K>& values);
+
+#endif
---
+++ ex03/src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "../inc/Vector.hpp"
+#include "../src/Vector.tpp"
+
+int main(void) {
+    std::cout << "LINEAR INTERPOLATION: \n";
+    try {
+        {
+            std::vector<float> vec_u = {0., 0.};
+            std::vector<float> vec_v = {1., 1.};
+            Vector u(vec_u);
+            Vector v(vec_v);
+            std::cout << "Vectors:\n" << u.dot(v) << std::endl;
+        }
+        {
+            std::vector<float> vec_u = {1., 1.};
+            std::vector<float> vec_v = {1., 1.};
+            Vector u(vec_u);
+            Vector v(vec_v);
+            std::cout << "Vectors:\n" << u.dot(v) << std::endl;
+        }
+        {
+            std::vector<float> vec_u = {-1., 6.};
+            std::vector<float> vec_v = {3., 2.};
+            Vector u(vec_u);
+            Vector v(vec_v);
+            std::cout << "Vectors:\n" << u.dot(v) << std::endl;
+        }
+        
+    } catch (std::runtime_error& e){
+        std::cout << e.what() << std::endl; 
+        return (1);
+    }
+    
+    return (0);
+}
