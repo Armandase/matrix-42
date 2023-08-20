7b7c066055d189d09dbe01d2281b7f5d78bc500d
Armandase <adamiens@student.42angouleme.fr>
2023-08-20 17:36:43 +0200

    ex10 in progress ;>

---
+++ ex10/src/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+#include "../inc/Matrix.hpp"
+#include "../src/Matrix.tpp"
+
+int main(void) {
+    std::cout << "Trace: \n";
+    try {
+        {
+            std::vector<std::vector<float> > matrix_u = {
+                {0., 0., 1.},
+                {0., 1., 0.},
+                {1., 0., 0.},
+            };
+            Matrix u(matrix_u);
+
+            std::cout << u.transpose() << std::endl;
+        }
+        // {
+        //     std::vector<std::vector<float> > matrix_u = {
+        //         {1., 0., 0.},
+        //         {0., 1., 0.},
+        //         {0., 0., 1.},
+        //     };
+        //     Matrix u(matrix_u);
+
+        //     std::cout << u.transpose() << std::endl;
+        // }
+        // {
+        //     std::vector<std::vector<float> > matrix_u = {
+        //         {1., 2.,},
+        //         {3., 4.,},
+        //     };
+        //     Matrix u(matrix_u);
+
+        //     std::cout << u.transpose() << std::endl;
+        // }
+        // {
+        //     std::vector<std::vector<float> > matrix_u = {
+        //         {1., 2.,},
+        //         {2., 4.,}
+        //     };
+        //     Matrix u(matrix_u);
+
+        //     std::cout << u.transpose() << std::endl;
+        // }
+        // {
+        //     std::vector<std::vector<float> > matrix_u = {
+        //         {8., 5., -2., 4., 28.},
+        //         {4., 2.5, 20., 4., -4.},
+        //         {8., 5., 1., 4., 17.},
+        //     };
+        //     Matrix u(matrix_u);
+
+        //     std::cout << u.transpose() << std::endl;
+        // }
+    } catch (std::runtime_error& e){
+        std::cout << e.what() << std::endl; 
+        return (1);
+    }
+    
+    return (0);
+}
